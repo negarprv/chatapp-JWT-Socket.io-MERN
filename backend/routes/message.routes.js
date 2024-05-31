f21af8883be18cfe5c836aeecaf17db7fd49f82d
Negar Parvaresh <negar.prv@gmail.com>
2024-05-31 21:28:09 +0330

    create  message

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
+import protectRoute from "../middelware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import connectToDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
